cbf9542dc7e17be2770666c0cac385f879d908e9
ZuzanaAK <kubatovazuzana@gmail.com>
2020-10-27 13:36:35 +0000

    the rocks are moving faster when reached 4 in score

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -49,6 +49,7 @@ let isRightArrow = false;
 let isLeftArrow = false;
 
 let score = 0;
+let intervalId = 0; 
 
 let rocks = [ { x: 500, y: 150 } ];
 let rockX = rocks.x;
@@ -66,9 +67,9 @@ let mammutY = 150;
 let mammutWidth = 50;
 let mammutHeight = 50;
 
-let mammutsYincrement = 2;
-let mammutsXincrement = 4;
-let mammutsYincrementMore = 5;
+let rocksYincrement = 3;
+let rocksXincrement = 5;
+let rocksYincrementMore = 5;
 
 touchedMammut = [];
 
@@ -150,23 +151,26 @@ updateCanvas = () => {
 //..............draw the mammut...............// 
 
     const drawMammut = () => {
- 
+        
     for (let i = 0; i < mammuts.length; i++) { 
+        if (mammuts[i]!== null) {
+
         ctx.drawImage(mammut, mammuts[i].x, mammuts[i].y);
         }
     }
-
+}
     
 //..............rock colision...............// 
 
     const collisionRocks = () => {
         
         for (let i = 0; i < rocks.length; i++) { 
-            if (caveManX < rocks[i].x + rockWidth &&
-                caveManX + caveManWidth > rocks[i].x &&
-                caveManY < rocks[i].y + rockHeight &&
-                caveManY + caveManHeight > rocks[i].y) 
-                {                    
+            if (caveManX < (rocks[i].x) + rockWidth &&
+                caveManX + caveManWidth > (rocks[i].x) &&
+                caveManY < (rocks[i].y) + rockHeight &&
+                caveManY + caveManHeight > (rocks[i].y)) 
+                {   
+                   //?clearInterval(intervalId);                
                     gameOver();
                 }
         }
@@ -190,8 +194,8 @@ updateCanvas = () => {
                 if (score === 10) {
                     winGame()
                 }
-
-                //mammuts[i] = null
+                // I need to get rid of the picture ones it touches caveman
+               // mammuts[i] = null
                 } 
             } 
         } 
@@ -201,7 +205,7 @@ updateCanvas = () => {
     const addRocks = () => {
         if (score < 8) {
             let randomPossibility = Math.floor(Math.random() * 190) 
-            let randomPlace = Math.floor(Math.random() * (canvas.width  - 30))
+            let randomPlace = Math.floor(Math.random() * (canvas.width  - 40))
             if (randomPossibility === 1) {
                 var rock = {
                     x: randomPlace,
@@ -211,7 +215,7 @@ updateCanvas = () => {
              
             } else {
                 let randomPossibility = Math.floor(Math.random() * 60) 
-                let randomPlace = Math.floor(Math.random() * (canvas.width  - 30))
+                let randomPlace = Math.floor(Math.random() * (canvas.width  - 40))
 
             if (randomPossibility === 1) {
                 var rock = {
@@ -242,9 +246,17 @@ updateCanvas = () => {
 //..............move rocks and level it up...............// 
 
     const moveRocks = () => {
-        rocks.forEach((rocks) => {
+        rocks.forEach((rock) => {
             if (rock !== null) {
                 rocks.y++}
+                        
+            if (score <= 3) {
+                rock.y++
+            } else if (score > 3 && score <= 7 ) {
+                rock.y += rocksYincrement
+            } else {
+                rock.y += rocksYincrementMore
+            }   
         })
     }
 
@@ -255,13 +267,6 @@ updateCanvas = () => {
             if (mammuts !== null) {
                 mammuts.y++
             }
-            /*if (score <= 4) {
-                mammuts.y++
-            } else if (score > 4 && score <= 7 ) {
-                mammuts.y += mammutYincrement
-            } else {
-                mammuts.y += mammutYincrementMore
-            }*/
         })
     }
  //..............game-over screen...............// 
