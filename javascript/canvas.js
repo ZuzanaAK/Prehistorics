e1474e668bdcaa2414c08f483dee766c42559d98
ZuzanaAK <kubatovazuzana@gmail.com>
2020-10-30 13:54:27 +0000

    when caveman touches a mammut, they vanish. sound effects added. styling of winscreen and gameover screen done.

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -36,7 +36,26 @@ let mammut = new Image ()
 mammut.src = "./resources/images/mammut.png"
 
 //..............audio...............//
-//add sounds later
+const yesSound = () => {
+    let yes = new Audio ();
+    yes.src = "./resources/sounds/yes.wav"
+    yes.volume = "0.7";
+    yes.play()
+}
+
+const oOh = () => {
+    let oo = new Audio ();
+    oo.src = "./resources/sounds/o-oh.mp3"
+    oo.volume = "0.7";
+    oo.play()
+}
+
+const youWon = () => {
+    let won = new Audio ();
+    won.src = "./resources/sounds/youWon.mp3"
+    won.volume = "0.7";
+    won.play()
+}
 
 //..............variables...............//
 let caveManX = 250;
@@ -49,6 +68,7 @@ let isRightArrow = false;
 let isLeftArrow = false;
 
 let score = 0;
+let intervalId = 0; 
 
 let rocks = [ { x: 500, y: 150 } ];
 let rockX = rocks.x;
@@ -66,11 +86,10 @@ let mammutY = 150;
 let mammutWidth = 50;
 let mammutHeight = 50;
 
-let mammutsYincrement = 2;
-let mammutsXincrement = 4;
-let mammutsYincrementMore = 5;
+let rocksYincrement = 3;
+let rocksXincrement = 5;
+let rocksYincrementMore = 5;
 
-touchedMammut = [];
 
 
 //..............draw caveMan...............//
@@ -103,7 +122,7 @@ updateCanvas = () => {
 
     ctx.font = '22px Verdana'
     ctx.fillStyle = "white"
-    ctx.fillText('Score: ' + score, 250, 50)
+    ctx.fillText('mammoths: ' + score, 230, 50)
 
     requestAnimationFrame(updateCanvas)
   }
@@ -157,157 +176,164 @@ updateCanvas = () => {
     }
 
     
-//..............rock colision...............// 
+//..............rock collision...............// 
 
-    const collisionRocks = () => {
+const collisionRocks = () => {
         
-        for (let i = 0; i < rocks.length; i++) { 
-            if (caveManX < rocks[i].x + rockWidth &&
-                caveManX + caveManWidth > rocks[i].x &&
-                caveManY < rocks[i].y + rockHeight &&
-                caveManY + caveManHeight > rocks[i].y) 
-                {                    
-                    gameOver();
-                }
-        }
-    } 
-
-//..............mammut colision...............//
-
-    const collisionMammuts = (i) => {
-
-        for (let i = 0; i < mammuts.length; i++) {
-            if (caveManX < mammuts[i].x + mammutWidth &&
-                caveManX + caveManWidth >= mammuts[i].x &&
-                caveManY < mammuts[i].y + mammutHeight &&
-                caveManY + caveManHeight > mammuts[i].y) 
-                {
+    for (let i = 0; i < rocks.length; i++) { 
+        if (caveManX < (rocks[i].x) + rockWidth &&
+            caveManX + caveManWidth > (rocks[i].x) &&
+            caveManY < (rocks[i].y) + rockHeight &&
+            caveManY + caveManHeight > (rocks[i].y)) 
+            {   
+               clearInterval(intervalId);
+                               
+                gameOver();
                 
-                if (!touchedMammut.includes(i)) {    
-                    score++
-                    touchedMammut.push(i)
-                
+            }
+    }
+} 
+
+//..............mammut collision...............//
+
+const collisionMammuts = () => {
+
+    for (let i = 0; i < mammuts.length; i++) {
+        if (caveManX < (mammuts[i].x) + mammutWidth &&
+            caveManX + caveManWidth >= (mammuts[i].x) &&
+            caveManY < (mammuts[i].y) + mammutHeight &&
+            caveManY + caveManHeight > (mammuts[i].y)) 
+            {
+            
+                yesSound(); 
+                score++;
+                mammuts.splice(i,1)
+
                 if (score === 10) {
                     winGame()
                 }
-
-                //mammuts[i] = null
-                } 
-            } 
         } 
-    }
+    } 
+}
 //..............add rocks...............// 
 
-    const addRocks = () => {
-        if (score < 8) {
-            let randomPossibility = Math.floor(Math.random() * 190) 
-            let randomPlace = Math.floor(Math.random() * (canvas.width  - 30))
-            if (randomPossibility === 1) {
-                var rock = {
-                    x: randomPlace,
-                    y: 10
-                }
-                    rocks.push(rock)}
-             
-            } else {
-                let randomPossibility = Math.floor(Math.random() * 60) 
-                let randomPlace = Math.floor(Math.random() * (canvas.width  - 30))
+const addRocks = () => {
+    if (score < 8) {
+        let randomPossibility = Math.floor(Math.random() * 190) 
+        let randomPlace = Math.floor(Math.random() * (canvas.width  - 60))
+        if (randomPossibility === 1) {
+            var rock = {
+                x: randomPlace,
+                y: 10
+            }
+                rocks.push(rock)}
+         
+        } else {
+            let randomPossibility = Math.floor(Math.random() * 60) 
+            let randomPlace = Math.floor(Math.random() * (canvas.width  - 60))
 
-            if (randomPossibility === 1) {
-                var rock = {
-                    x: randomPlace,
-                    y: 10
-                }
-                    rocks.push(rock);
-            } 
-        }
+        if (randomPossibility === 1) {
+            var rock = {
+                x: randomPlace,
+                y: 10
+            }
+                rocks.push(rock);
+        } 
     }
+}
 
 //..............add mammuts...............// 
 
-    const addMammut = () => {
-        let randomPossibility = Math.floor(Math.random() * 180) 
-        let randomPlace = Math.floor(Math.random() * (canvas.width - 30))
-            if(randomPossibility === 1){
-                var mammut = {
-                    x: randomPlace,
-                    y: 10
-                }
+const addMammut = () => {
+    let randomPossibility = Math.floor(Math.random() * 180) 
+    let randomPlace = Math.floor(Math.random() * (canvas.width - 60))
+        if(randomPossibility === 1){
+            var mammut = {
+                x: randomPlace,
+                y: 10
+            }
 
-            mammuts.push(mammut);
-        } 
-    }
+        mammuts.push(mammut);
+    } 
+}
 
 
 //..............move rocks and level it up...............// 
 
-    const moveRocks = () => {
-        rocks.forEach((rocks) => {
-            if (rock !== null) {
-                rocks.y++}
-        })
-    }
+const moveRocks = () => {
+    rocks.forEach((rock) => {
+        if (rock !== null) {
+            rocks.y++}
+                    
+        if (score <= 3) {
+            rock.y++
+        } else if (score > 3 && score <= 7 ) {
+            rock.y += rocksYincrement
+        } else {
+            rock.y += rocksYincrementMore
+        }   
+    })
+}
 
-//..............move mammuts and level it up...............//    
+//..............move mammuts...............//    
 
-    const moveMammut = () => {
-        mammuts.forEach((mammuts) => {
-            if (mammuts !== null) {
-                mammuts.y++
-            }
-            /*if (score <= 4) {
-                mammuts.y++
-            } else if (score > 4 && score <= 7 ) {
-                mammuts.y += mammutYincrement
-            } else {
-                mammuts.y += mammutYincrementMore
-            }*/
-        })
-    }
- //..............game-over screen...............// 
+const moveMammut = () => {
+    mammuts.forEach((mammuts) => {
+        if (mammuts !== null) {
+            mammuts.y++
+        }
+    })
+}
+//..............game-over screen...............// 
 
-    const gameOver = () => {
-        let canvas = document.querySelector("canvas")
-        canvas.className = "hidden"
+const gameOver = () => {
+    oOh(); 
+    let canvas = document.querySelector("canvas")
+    canvas.className = "hidden"
 
-        let gameOverScreen = document.createElement("div")
-        gameOverScreen.innerHTML =`
+    let gameOverScreen = document.createElement("div")
+    gameOverScreen.className = "gameoverscreen"
+    gameOverScreen.innerHTML =`
+    <body id="body">
+    <div id="splash">
+    <section class="content" id="margin">
+        <h2>Bummer, you lost!</h2>
+        <h5>- no man, caveman included, can survive being hit by such a rock! -</h5>
+        <p>- don't give up and try again -</p>
+        <button id="start-btn" class="btn btn-warning" type="button" onclick="location.href='index.html'"><span id="play">PLAY ONCE MORE!</span></button>
+        <img id="gameOver"src="./resources/giphy/game-over_gif.gif" alt="" width="300" height="200">
+    </section>
+    </div>
+    </body>
+    `
+    let body = document.querySelector("body")
+    canvas.parentNode.removeChild(canvas)
+    body.appendChild(gameOverScreen)
+}
+
+//..............winning screen...............//     
+
+const winGame = () => {
+    youWon()
+    let canvas = document.querySelector("canvas")
+    canvas.className = "hidden"
+
+    let winGame = document.createElement("div")
+    winGame.className = "winscreen"
+    winGame.innerHTML = `
         <body id="body">
         <div id="splash">
         <section class="content" id="margin">
-            <h2>Bummer, you lost!</h2>
-            <h5>- no man, caveman included, can survive being hit by such a rock! -</h5>
-            <p>- don't give up and try again -</p>
-            <button id="start-btn" id="button" onclick="location.href='index.html'"><span id="play">PLAY ONCE MORE!</span></button>
+            <h2>Congratulations, YOU WON!</h2>
+            <h5>- you caught all the mammoths, get ready for a party in your cave! -</h5>
+            <p>- good job! -</p>
+            <button id="start-btn" class="btn btn-warning" type="button" onclick="location.href='index.html'"><span id="play">PLAY ONCE MORE!</span></button>
+            <img id="winGame" src="./resources/giphy/game-win_gif.gif" alt="" width="300" height="200">
         </section>
         </div>
         </body>
         `
-        let body = document.querySelector("body")
-        canvas.parentNode.removeChild(canvas)
-        body.appendChild(gameOverScreen)
-    }
-
- //..............winning screen...............//     
-
-    const winGame = () => {
-        let canvas = document.querySelector("canvas")
-        canvas.clasName = "hidden"
-
-        let winGame = document.createElement("div")
-        winGame.innerHTML = `
-            <body id="body">
-            <div id="splash">
-            <section class="content" id="margin">
-                <h2>Congratulations, YOU WON!</h2>
-                <h5>- all the mammuts have been caught and are ready to be brought to your cave -</h5>
-                <p>- good job! -</p>
-                <button id="start-btn" id="button" onclick="location.href='index.html'"><span id="play">PLAY ONCE MORE!</span></button>
-            </section>
-            </div>
-            </body>
-            `
-        let body = document.querySelector("body")
-        canvas.parentNode.removeChild(canvas)
-        body.appendChild(winGame)
-    }
+    let body = document.querySelector("body")
+    canvas.parentNode.removeChild(canvas)
+    body.appendChild(winGame)
+}
